7d33a28341de2dd55a3afab3ed3551c7de57b1a7
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2023-04-03 23:48:59 +0700

    first commit

---
+++ ALGORITMA/1.js
@@ -0,0 +1,24 @@
+/*
+Terdapat string "NEGIE1", silahkan reverse alphabet nya dengan angka tetap diakhir kata Hasil = "EIGEN1"
+*/
+
+const str = "NEGIE1";
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const number = 
+
+
+// const str = "NEGIE1";
+// const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const reversed = str
+//   .split("")
+//   .map((char) => {
+//     if (alphabet.includes(char)) {
+//         console.log('ini adalah ', char);
+//       const index = alphabet.indexOf(char);
+//       return alphabet[25 - index];
+//     }
+//     return char;
+//   })
+//   .join("");
+
+// console.log(reversed);
