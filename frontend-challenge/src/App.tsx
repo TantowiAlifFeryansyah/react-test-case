350ebbe0c9141ff0421cdc6a55aac6d4fa87f03d
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2023-04-06 23:26:03 +0700

    done

--- frontend-challenge/src/App.tsx
+++ frontend-challenge/src/App.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import getNews from './newsAPI';
-import { List, Card } from 'antd';
+import { List, Card, Typography, Image } from 'antd';
+const { Paragraph, Text } = Typography;
 
 interface Article {
   title: string;
+  description: string;
   url: string;
+  urlToImage: string;
+  author: string,
 }
 
 function App() {
@@ -21,14 +25,36 @@ function App() {
 
   return (
     <div>
-      <h1>News Headlines</h1>
+      <h1 style={{textAlign: 'center', fontSize: 40, marginBottom: 50, marginTop: 30}}>News Headlines</h1>
       <List
         grid={{ gutter: 16, column: 3 }}
         dataSource={news}
         renderItem={(item) => (
           <List.Item>
-            <Card title={item.title}>
-              <a href={item.url}>Read More</a>
+            <Card title={item.title} bordered={true} style={{ width: '600px', height: '570px', textAlign: 'center'}}>
+              <div>
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', paddingBottom: 10}}>
+                  <Image
+                    src={item.urlToImage}
+                    style={{ height: '300px',objectFit: 'cover'}}
+                  />
+                </div>
+
+                <div style={{marginTop: 10, paddingBottom: 10, height: '80px'}}>
+                  <Paragraph>{item.description}</Paragraph>
+                </div>
+
+                <div>
+                  <a href={item.url}>Read More</a>
+                </div>
+
+                <div>
+                  <div style={{fontSize: 0}}>
+                  <Text>Author by: {item.author ? item.author : "anonym"}</Text>
+                  </div>
+                </div>
+              </div>
+
             </Card>
           </List.Item>
         )}
--- frontend-challenge/src/newsAPI.ts
+++ frontend-challenge/src/newsAPI.ts
@@ -4,18 +4,22 @@ const apiKey: string = 'da29de7e67e64cea91fd39cff9482167';
 const apiUrl: string = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 
 interface Article {
-  title: string;
-  url: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    author: string,
+
 }
 
 const getNews = async (): Promise<Article[]> => {
-  try {
-    const response = await axios.get(apiUrl);
-    return response.data.articles;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
+    try {
+        const response = await axios.get(apiUrl);
+        return response.data.articles;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
 };
 
 export default getNews;
